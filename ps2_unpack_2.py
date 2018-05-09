7cf2c99b95ed0ec828531d4732557d1f215d2425
PriyankReddy <Priyank.rc@gmail.com>
2018-05-08 23:09:37 -0700

    Update ps2_unpack_2.py

--- ps2_unpack_2.py
+++ ps2_unpack_2.py
@@ -9,3 +9,23 @@ name, email, *phone_numbers = record
 print('Name: {}'.format(name))
 print('Email: {}'.format(email))
 print('Phone Numbers: {}'.format(phone_numbers))
+
+''' example #2 '''
+
+records = [
+     ('foo', 1, 2),
+     ('bar', 'hello'),
+     ('foo', 3, 4),
+]
+
+def do_foo(x, y):
+    print('foo', x, y)
+
+def do_bar(s):
+    print('bar', s)
+
+for tag, *args in records:
+    if tag == 'foo':
+        do_foo(*args)
+    elif tag == 'bar':
+        do_bar(*args)
