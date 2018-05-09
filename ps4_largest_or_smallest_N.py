dca4f7fc0969f3308e1a6c5f8e7849529c915ec0
PriyankReddy <Priyank.rc@gmail.com>
2018-05-09 00:58:18 -0700

    Update ps4_largest_or_smallest_N.py

--- ps4_largest_or_smallest_N.py
+++ ps4_largest_or_smallest_N.py
@@ -24,3 +24,21 @@ cheap = heapq.nsmallest(3, portfolio, key=lambda s: s['price'])
 print('Cheap: {}'.format(cheap))
 expensive = heapq.nlargest(3, portfolio, key=lambda s: s['price'])
 print('Expensive: {}'.format(expensive))
+
+''' example #3 '''
+
+import heapq
+
+nums = (1, 8, 2, 23, 7, -4, 18, 23, 42, 37, 2)
+h = list(nums)
+heapq.heapify(h)        # converts a list to heap. argument must be a list
+print('h: {}'.format(h))
+heapq.heappop(h)        # pops the smallest element out of heap
+print('h: {}'.format(h))
+heapq.heappush(h,99)    # pushes the new element into heap
+print('h: {}'.format(h))
+heapq.heappushpop(h,1)  # pushes the new element first then pops smallest element 
+print('h: {}'.format(h))
+heapq.heapreplace(h,1)  # pops smallest element first then pushes the new element
+print('h: {}'.format(h))
+
